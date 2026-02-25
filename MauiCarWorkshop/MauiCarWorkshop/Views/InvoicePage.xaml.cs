698aa0b78e48304ef598381d507d3535f8c4c25d
Nicolai <nico368f@gmail.com>
2026-02-25 13:59:49 +0100

    Added an InvoicePage and viewModel. Added nuget package CommunityToolkit.Maui and CommunityToolKit.Mvvm

---
+++ MauiCarWorkshop/MauiCarWorkshop/Views/InvoicePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiCarWorkshop.Views;
+
+public partial class InvoicePage : ContentPage
+{
+    public InvoicePage()
+    {
+        InitializeComponent();
+    }
+}
