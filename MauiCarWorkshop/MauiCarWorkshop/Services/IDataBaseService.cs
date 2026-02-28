7aa62ceae2f34adfd2e6db53091c78b0fcec5ad4
Nicolai <nico368f@gmail.com>
2026-02-28 13:04:37 +0100

    Expanded the interface for the database with full CRUD

--- MauiCarWorkshop/MauiCarWorkshop/Services/IDataBaseService.cs
+++ MauiCarWorkshop/MauiCarWorkshop/Services/IDataBaseService.cs
@@ -9,9 +9,13 @@ public interface IDatabaseService
     Task<List<Order>> GetOrdersAsync();
     Task<Order?> GetOrderByIdAsync(int id);
     Task<List<Order>> GetOrdersByDateAsync(DateTime date);
+    Task<int> UpdateOrderAsync(Order order);
+    Task<int> DeleteOrderAsync(Order order); 
 
     // Invoices
     Task<int> AddInvoiceAsync(Invoice invoice);
     Task<List<Invoice>> GetInvoicesAsync();
     Task<Invoice?> GetInvoiceByIdAsync(int id);
+    Task<int> UpdateInvoiceAsync(Invoice invoice);
+    Task<int> DeleteInvoiceAsync(Invoice invoice);
 }
