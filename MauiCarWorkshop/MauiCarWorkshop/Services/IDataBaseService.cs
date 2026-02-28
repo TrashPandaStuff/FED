cc9b5edb2b71a9b813f13895d040a6669a2f259a
Nicolai <nico368f@gmail.com>
2026-02-28 12:59:08 +0100

    Wrote interface for the database

--- MauiCarWorkshop/MauiCarWorkshop/Services/IDataBaseService.cs
+++ MauiCarWorkshop/MauiCarWorkshop/Services/IDataBaseService.cs
@@ -1,6 +1,17 @@
-﻿namespace MauiCarWorkshop.Services;
+﻿using MauiCarWorkshop.Models;
 
-public interface IDataBaseService
+namespace MauiCarWorkshop.Services;
+
+public interface IDatabaseService
 {
-    
+    // Orders
+    Task<int> AddOrderAsync(Order order);
+    Task<List<Order>> GetOrdersAsync();
+    Task<Order?> GetOrderByIdAsync(int id);
+    Task<List<Order>> GetOrdersByDateAsync(DateTime date);
+
+    // Invoices
+    Task<int> AddInvoiceAsync(Invoice invoice);
+    Task<List<Invoice>> GetInvoicesAsync();
+    Task<Invoice?> GetInvoiceByIdAsync(int id);
 }
