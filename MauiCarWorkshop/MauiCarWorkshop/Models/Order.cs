153054d15fbb75a8517c7c17b7afd3193998bf8a
Nicolai <nico368f@gmail.com>
2026-02-28 12:35:09 +0100

    Defined the order data model

--- MauiCarWorkshop/MauiCarWorkshop/Models/Order.cs
+++ MauiCarWorkshop/MauiCarWorkshop/Models/Order.cs
@@ -1,6 +1,15 @@
-﻿namespace MauiCarWorkshop.Models;
+﻿using SQLite;
+
+namespace MauiCarWorkshop.Models;
 
 public class Order
 {
-    
+    [PrimaryKey, AutoIncrement] public int OrderId { get; set; }
+    public required string CustomerName { get; set; }
+    public required string Address { get; set; }
+    public required string CarBrand { get; set; }
+    public required string CarModel { get; set; }
+    public required string LicensePlateNumber { get; set; }
+    public required DateTime DeliveryDate { get; set; }
+    public required string TaskDescription { get; set; }
 }
