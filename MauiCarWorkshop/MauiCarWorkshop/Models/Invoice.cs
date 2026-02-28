c686302ee2365eb2e7f8471c330c243c981d0184
Nicolai <nico368f@gmail.com>
2026-02-28 12:39:40 +0100

    Created the invoice data model

--- MauiCarWorkshop/MauiCarWorkshop/Models/Invoice.cs
+++ MauiCarWorkshop/MauiCarWorkshop/Models/Invoice.cs
@@ -1,6 +1,24 @@
-﻿namespace MauiCarWorkshop.Models;
+﻿using SQLite;
+
+namespace MauiCarWorkshop.Models;
 
 public class Invoice
 {
+    [PrimaryKey, AutoIncrement] 
+    public int InvoiceId { get; set; }
     
+    public required int OrderId { get; set; } 
+
+    public required string MechanicName { get; set; }
+
+    public required string MaterialsUsed { get; set; }
+
+    public required decimal MaterialCost { get; set; }
+
+    public required decimal HoursWorked { get; set; }
+
+    public required decimal HourlyRate { get; set; }
+
+    public required decimal TotalPrice { get; set; }
+}
 }
--- MauiCarWorkshop/MauiCarWorkshop/Models/Order.cs
+++ MauiCarWorkshop/MauiCarWorkshop/Models/Order.cs
@@ -5,11 +5,18 @@ namespace MauiCarWorkshop.Models;
 public class Order
 {
     [PrimaryKey, AutoIncrement] public int OrderId { get; set; }
+    
     public required string CustomerName { get; set; }
+    
     public required string Address { get; set; }
+    
     public required string CarBrand { get; set; }
+    
     public required string CarModel { get; set; }
+    
     public required string LicensePlateNumber { get; set; }
+    
     public required DateTime DeliveryDate { get; set; }
+    
     public required string TaskDescription { get; set; }
 }
